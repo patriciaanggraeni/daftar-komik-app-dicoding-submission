844aa9bf753042bfe8227505396c0e67f7c510a6
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-17 10:10:12 +0700

    initial project & install dependencies

---
+++ app/src/main/java/com/example/aplikasi_sederhana/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasi_sederhana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
